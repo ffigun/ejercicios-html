94ceabf8d56549fe9ff9bcae03084a95832cbf71
Fernando Figún <63000403+ffigun@users.noreply.github.com>
2021-11-22 20:09:03 -0300

    Add files via upload

---
+++ final/js/main.js
@@ -0,0 +1,3 @@
+function enviarFormulario() {
+  alert ("¡Oops! Esta función se encuentra en mantenimiento.\n\nTe invitamos a acceder al menú Contacto donde habilitamos un método alternativo.\n\n¡Muchas gracias!");
+}
